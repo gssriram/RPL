d1a64913f38436bd176682cda1383b6cd9a7903f
Sriram G S <60818433+gssriram@users.noreply.github.com>
2023-09-27 20:16:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,42 @@
 import gradio as gr
 import time
 from transformers import pipeline
+from gtts import gTTS
 
 # Initialize the chatbot model
 pipe = pipeline("text-generation", model="sriramgs/RPL_gpt2_new")
 asr = pipeline("automatic-speech-recognition", model="openai/whisper-small")
-
+global bot_message
 # Create a Gradio interface
 with gr.Blocks() as demo:
-    play_audio = gr.Button("Play the Chatbot response")
 
-    chatbot = gr.Chatbot()
-    msg = gr.Textbox(placeholder="Ask me your doubts")
-    audio_file = [gr.Audio(source="microphone",type="filepath",optional=True),]
-    record_audio = gr.Button("Convert into Text")
-    clear = gr.Button("Clear the Chatbot Conversation")
+    chatbot = gr.Chatbot(avatar_images=("human.png", "bot.png"), value=[[None, "Welcome to the Indore-Ekk Number Superstore family! We're thrilled to have you on board. \n How can I assist you today?"]])
+    with gr.Row(label="Voice Input and Output"):
+        with gr.Column(variant="panel"):
+            audio_file = gr.Audio(label='Voice based Input',source="microphone",type="filepath",optional=True)
+        with gr.Column(variant="panel"):
+            play_audio = gr.Audio(label='Output Audio', autoplay=True)
+    audio_out = gr.Textbox(visible=False)
 
-    def text_to_speech():
-      pass
+    with gr.Row(label="Voice Input and Output"):
+        with gr.Column(label='Text Based Input', variant="panel"):
+            msg = gr.Textbox(placeholder="Ask me your doubts")
+        with gr.Column(variant="panel"):
+          with gr.Row():
+            clear = gr.Button("Clear the Chatbot Conversation")
+
+    def text_to_speech(text):
+      var = gTTS(text = text,lang = 'en')
+      var.save('eng.mp3')
+      return gr.Audio.update(value='eng.mp3')
 
     def user(user_message, history):
         global query
+        global fck
         query = user_message
-        return "", history + [[user_message, None]]
+        fck = model_response(query)
+        print(user_message,fck)
+        return '', history + [[user_message, None]],gr.Textbox.update(value=fck)
 
     def model_response(query):
         global a
@@ -31,30 +45,36 @@ with gr.Blocks() as demo:
         return a
 
     def bot(history):
-        bot_message = model_response(query)
-        history[-1][1] = ""
-        for character in bot_message:
-            history[-1][1] += character
-            time.sleep(0.05)
-            yield history
+      global bot_message
+      bot_message = model_response(query)
+      history[-1][1] = ""
+      for character in fck:
+          history[-1][1] += character
+          time.sleep(0.05)
+          yield history
 
-    def speech_to_text(mic=None, file=None):
-      if mic is not None:
-          audio = mic
-      elif file is not None:
-          audio = file
+
+    def speech_to_text(audio_file,history):
+      if audio_file == None:
+        return "", history + [[None, None]]
       else:
-          return "You must either provide a mic recording or a file"
-      text = asr(audio)["text"]
-      return text
+        global query
+        global fck
+        text = asr(audio_file)["text"]
+        query = text
+        fck = model_response(query)
+        print(text)
+        return None, history + [[text, None]],gr.Textbox.update(value=fck)
+        #return text
 
-    msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
+    audio_file.stop_recording(speech_to_text, [audio_file,chatbot], [audio_file,chatbot,audio_out], queue=False, show_progress=False).then(bot, chatbot, chatbot)
+
+    msg.submit(user, [msg, chatbot], [msg, chatbot,audio_out], queue=False).then(
         bot, chatbot, chatbot
     )
 
     clear.click(lambda: None, None, chatbot, queue=False)
-    record_audio.click(speech_to_text,inputs=audio_file,outputs=msg)
-    play_audio.click(text_to_speech)
+    audio_out.change(text_to_speech,inputs=[audio_out], outputs=play_audio)
 
 demo.queue()
 demo.launch(debug=True)
